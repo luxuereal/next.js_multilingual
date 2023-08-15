a4c4112428727d5696bd4073397bcd156ece2cd5
Shinee Hawk <130590142+shinyhawk@users.noreply.github.com>
2023-08-15 16:08:25 -0400

    fix sample text

--- app/[locale]/blog-details/page.tsx
+++ app/[locale]/blog-details/page.tsx
@@ -151,7 +151,7 @@ const BlogDetailsPage = () => {
                     );
                   })}
 
-                  <div className="relative z-10 mb-10 overflow-hidden rounded-md bg-primary bg-opacity-10 p-8 md:p-9 lg:p-8 xl:p-9">
+                  {/* <div className="relative z-10 mb-10 overflow-hidden rounded-md bg-primary bg-opacity-10 p-8 md:p-9 lg:p-8 xl:p-9">
                     <p className="text-center text-base font-medium italic text-body-color">
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                       sed do eiusmod incididunt utionals labore et dolore magna
@@ -318,7 +318,7 @@ const BlogDetailsPage = () => {
                         <SharePost />
                       </div>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
